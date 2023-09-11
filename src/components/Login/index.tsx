934780b8414080d0b7d974a5972afd3354dbc27a
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-09-11 19:52:46 +0900

    :zap: :: web view

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
 import Logo from "../../../assets/Logo.svg";
+import { WithLocalSvg } from "react-native-svg";
 import { View, Text, TextInput, Button, Alert } from "react-native";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #f5f5f5;
+  background-color: #fff;
+`;
+
+const ImageContainer = styled.View`
+  font-size: 26px;
+  margin-left: 50px;
+  margin-top: 20px;
+  font-weight: 600;
+`;
+
+const InputContainer = styled.View`
+  width: 100%;
+  align-items: center;
+`;
+
+const PasswordContainer = styled.View`
+  width: 100%;
+  align-items: center;
 `;
 
 const Input = styled.TextInput`
@@ -15,11 +33,39 @@ const Input = styled.TextInput`
   height: 40px;
   margin-bottom: 20px;
   padding: 10px;
-  border: 1px solid #ccc;
+  border: 1px solid black;
   border-radius: 5px;
+  border-width: 0 0 1px;
 `;
 
-const LoginButton = styled.Button``;
+const Label = styled.Text`
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+`;
+
+const ButtonContainer = styled.View`
+  width: 290px;
+  height: 50px;
+  border-radius: 30px;
+`;
+
+const LoginButton = styled.Button`
+  background-color: #82c96d;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
 
 const LoginScreen: React.FC = () => {
   const [id, setId] = useState("");
@@ -38,21 +84,34 @@ const LoginScreen: React.FC = () => {
 
   return (
     <Container>
-      <Text>로그인 화면</Text>
-      <Input
-        placeholder="아이디"
-        onChangeText={onChangeId}
-        value={id}
-        autoCapitalize="none"
-      />
-      <Input
-        placeholder="비밀번호"
-        onChangeText={onChangePw}
-        value={pw}
-        secureTextEntry
-        autoCapitalize="none"
-      />
-      <LoginButton title="로그인" onPress={login} />
+      <ImageContainer>
+        <WithLocalSvg width={200} height={40} asset={Logo} />
+      </ImageContainer>
+      <InputContainer>
+        <Label>이메일</Label>
+        <Input
+          placeholder="이메일을 입력하세요"
+          onSubmitEditing={login}
+          onChangeText={onChangeId}
+          value={id}
+          autoCapitalize="none"
+        />
+      </InputContainer>
+      <PasswordContainer>
+        <Label>비밀번호</Label>
+        <Input
+          placeholder="비밀번호를 입력하세요"
+          onChangeText={onChangePw}
+          value={pw}
+          secureTextEntry
+          autoCapitalize="none"
+        />
+      </PasswordContainer>
+      <ButtonContainer>
+        <LoginButton title="로그인" onPress={login}>
+          <ButtonText>로그인</ButtonText>
+        </LoginButton>
+      </ButtonContainer>
     </Container>
   );
 };
